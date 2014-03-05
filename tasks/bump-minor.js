dbe02ba4f847fcfcb33022c111fb1a9537702b07
Raynos <raynos2@gmail.com>
2014-03-05 13:38:14 -0800

    moved transact file to lib

--- tasks/bump-minor.js
+++ tasks/bump-minor.js
@@ -1,30 +1,20 @@
 var parallel = require('continuable-para');
-var fs = require('fs');
 var path = require('path');
 
-function transactFile(file, lambda, cb) {
-    fs.readFile(file, function (err, buf) {
-        if (err) {
-            return cb(err);
-        }
-
-        fs.writeFile(file, lambda(buf), cb);
-    });
-}
+var transactJsonFile = require('../lib/transact-json-file.js');
 
 function bumpMinor(opts, cb) {
-    function setVersion(buf) {
-        var package = JSON.parse(String(buf));
+    function setVersion(package) {
         package.version = opts.nextVersion;
-        return JSON.stringify(package, null, '  ');
+        return package;
     }
 
     var packageFile = path.join(opts.folder, 'package.json');
     var shrinkwrapFile = path.join(opts.folder, 'npm-shrinkwrap.json');
 
     parallel([
-        transactFile.bind(null, packageFile, setVersion),
-        transactFile.bind(null, shrinkwrapFile, setVersion)
+        transactJsonFile.bind(null, packageFile, setVersion),
+        transactJsonFile.bind(null, shrinkwrapFile, setVersion)
     ], cb);
 }
 
