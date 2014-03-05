a7f8608f433b0d5f87f808fb03f36677ed6d694d
Raynos <raynos2@gmail.com>
2014-03-05 14:49:11 -0800

    moved computeVersion logic into bump-minor

--- index.js
+++ index.js
@@ -1,42 +1,29 @@
 var bumpMinor = require('./tasks/bump-minor.js');
 var updateChangelog = require('./tasks/update-changelog.js');
 var commitChanges = require('./tasks/commit-changes.js');
-var computeVersion = require('./tasks/compute-next-version.js');
 
 function main(opts, cb) {
     if (typeof opts === 'string') {
         opts = { folder: opts };
     }
 
-    function next(err, nextVersion) {
+    bumpMinor(opts, function (err, nextVersion) {
         if (err) {
             return cb(err);
         }
 
-        opts.nextVersion = nextVersion;
+        opts.nextVersion = opts.nextVersion || nextVersion;
 
-        bumpMinor(opts, function (err) {
+        updateChangelog(opts, function (err) {
             if (err) {
                 return cb(err);
             }
 
-            updateChangelog(opts, function (err) {
-                if (err) {
-                    return cb(err);
-                }
-
-                commitChanges(opts, function (err) {
-                    cb(err, err ? null : nextVersion);
-                });
+            commitChanges(opts, function (err) {
+                cb(err, err ? null : nextVersion);
             });
         });
-    }
-
-    if (!opts.nextVersion) {
-        computeVersion(opts, next);
-    } else {
-        next(null, opts.nextVersion);
-    }
+    });
 }
 
 module.exports = main;
--- tasks/bump-minor.js
+++ tasks/bump-minor.js
@@ -1,21 +1,36 @@
 var parallel = require('continuable-para');
 var path = require('path');
 
+var computeVersion = require('./compute-version.js');
 var transactJsonFile = require('../lib/transact-json-file.js');
 
 function bumpMinor(opts, cb) {
-    function setVersion(package) {
-        package.version = opts.nextVersion;
-        return package;
+    function next(err, nextVersion) {
+        if (err) {
+            return cb(err);
+        }
+
+        function setVersion(package) {
+            package.version = nextVersion;
+            return package;
+        }
+
+        var packageFile = path.join(opts.folder, 'package.json');
+        var shrinkwrapFile = path.join(opts.folder, 'npm-shrinkwrap.json');
+
+        parallel([
+            transactJsonFile.bind(null, packageFile, setVersion),
+            transactJsonFile.bind(null, shrinkwrapFile, setVersion)
+        ], function (err) {
+            cb(err, err ? null : nextVersion);
+        });
     }
 
-    var packageFile = path.join(opts.folder, 'package.json');
-    var shrinkwrapFile = path.join(opts.folder, 'npm-shrinkwrap.json');
-
-    parallel([
-        transactJsonFile.bind(null, packageFile, setVersion),
-        transactJsonFile.bind(null, shrinkwrapFile, setVersion)
-    ], cb);
+    if (!opts.nextVersion) {
+        computeVersion(opts, next);
+    } else {
+        next(null, opts.nextVersion);
+    }
 }
 
 module.exports = bumpMinor;
