11c6f979fa4ff2080ab4e99f39c1b70c50fbabca
Raynos <raynos2@gmail.com>
2014-03-05 14:08:50 -0800

    use default values

--- bin/install.js
+++ bin/install.js
@@ -7,17 +7,13 @@ function installModule(opts, cb) {
     var file = path.join(opts.folder, 'package.json');
 
     transactJsonFile(file, function (package) {
-        if (!package.scripts) {
-            package.scripts = {};
-        }
+        package.scripts = package.scripts || {};
 
         if (!package.scripts.changelog) {
             package.scripts.changelog = 'build-changelog';
         }
 
-        if (!package.devDependencies) {
-            package.devDependencies = {};
-        }
+        package.devDependencies = package.devDependencies || {};
 
         package.devDependencies['build-changelog'] =
             '~' + version;
