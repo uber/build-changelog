104cf84c5610dc9f5512de45144076e2e248f66d
Raynos <raynos2@gmail.com>
2014-04-01 16:43:54 -0700

    update the install command to add more run scripts

--- bin/install.js
+++ bin/install.js
@@ -3,20 +3,34 @@ var path = require('path');
 var version = require('../package.json').version;
 var transactJsonFile = require('../lib/transact-json-file.js');
 
+var minorCommand = 'build-changelog --minor';
+var patchCommand = 'build-changelog --patch';
+var deprecatedCommand = 'echo DEPRECATED: please run either ' +
+    'of the changelog-patch or changelog-minor scripts; exit 1';
+
 function installModule(opts, cb) {
     var file = path.join(opts.folder, 'package.json');
 
     transactJsonFile(file, function (package) {
         package.scripts = package.scripts || {};
 
-        if (!package.scripts.changelog) {
-            package.scripts.changelog = 'build-changelog';
+        if (!package.scripts['changelog-minor']) {
+            package.scripts['changelog-minor'] = minorCommand;
         }
 
-        package.devDependencies = package.devDependencies || {};
+        if (!package.scripts['changelog-patch']) {
+            package.scripts['changelog-patch'] = patchCommand;
+        }
 
-        package.devDependencies['build-changelog'] =
-            '^' + version;
+        if (package.scripts.changelog) {
+            package.scripts.changelog = deprecatedCommand;
+        }
+
+        if (!opts.onlyScripts) {
+            package.devDependencies = package.devDependencies || {};
+            package.devDependencies['build-changelog'] =
+                '^' + version;
+        }
 
         return package;
     }, cb);
