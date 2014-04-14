1922db9cfa27adb76e916e184d8f81763e1f758a
Raynos <raynos2@gmail.com>
2014-04-14 13:01:35 -0700

    updated install to add major + refactoring

--- bin/install.js
+++ bin/install.js
@@ -1,26 +1,30 @@
 var path = require('path');
+var template = require('string-template');
 
 var version = require('../package.json').version;
 var transactJsonFile = require('../lib/transact-json-file.js');
 
-var minorCommand = 'build-changelog --minor';
-var patchCommand = 'build-changelog --patch';
+var majorCommand = '{cmd} --major';
+var minorCommand = '{cmd} --minor';
+var patchCommand = '{cmd} --patch';
 var deprecatedCommand = 'echo DEPRECATED: please run either ' +
     'of the changelog-patch or changelog-minor scripts; exit 1';
 
 function installModule(opts, cb) {
     var file = path.join(opts.folder, 'package.json');
 
+    opts.cmd = opts.cmd || 'build-changelog';
+    opts.packageVersion = opts.packageVersion || '^' + version;
+
     transactJsonFile(file, function (package) {
         package.scripts = package.scripts || {};
 
-        if (!package.scripts['changelog-minor']) {
-            package.scripts['changelog-minor'] = minorCommand;
-        }
-
-        if (!package.scripts['changelog-patch']) {
-            package.scripts['changelog-patch'] = patchCommand;
-        }
+        package.scripts['changelog-major'] =
+            template(majorCommand, opts);
+        package.scripts['changelog-minor'] =
+            template(minorCommand, opts);
+        package.scripts['changelog-patch'] =
+            template(patchCommand, opts);
 
         if (package.scripts.changelog) {
             package.scripts.changelog = deprecatedCommand;
@@ -28,8 +32,7 @@ function installModule(opts, cb) {
 
         if (!opts.onlyScripts) {
             package.devDependencies = package.devDependencies || {};
-            package.devDependencies['build-changelog'] =
-                '^' + version;
+            package.devDependencies[opts.cmd] = opts.packageVersion;
         }
 
         return package;
